1eddf42530d4f34b2035c07f820ad41057b264f5
dualhorn16 <dustin-horner@uiowa.edu>
2016-12-03 15:37:49 -0600

    Updates to trees.py/beta_redux.py
    Updated the beta reduction functions in trees.py and the hardcoded
    trees in beta_redux.py.  As of now the two hard coded values I test, do
    indeed reduce correctly, but more testing is needed with various trees
    to ensure proper operation and then implementation of corner cases that
    would loop forever.

--- beta_redux.py
+++ beta_redux.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #************************************************
 #	Lamda Calculus Beta Redux Calculator
 #	
@@ -12,35 +13,72 @@
 #	for storing lamda calculus expressions.
 #***********************************************/
 
-from trees import Tree
+from trees import *
 
 
 def main():
     
-    print "\n"
+    #EXAMPLE 1:
+    #this expression is "(lamda x.x)y"
     tree = Tree()
     root = None
     root = tree.insert(root, "@")
-    print root
     tree.insertLeft(root, "Lamda")
     tree.insertRight(root, "y")
     tree.insertLeft(root.left, "x")
     tree.insertRight(root.left, "x")
+    #tree created... now doing something meaningful.
 
     print "\n"
-    print "Traverse Inorder"
+    print "Traverse Inorder BEFORE:"
     tree.traverseInorder(root)
-    print tree.beta_redux_present(root)
+    if tree.beta_redux_present(root, False):
+        root = tree.perform_beta_redux(root)
+    else:
+        print "no beta redux present"
+    print "\nTraverse Inorder AFTER:"
+    tree.traverseInorder(root)
+    print "++++++++++++++++++++++\n\n"
 
+
+    #EXAMPLE 2:
+    #This expression (λf.λx.fx)λy.y+1 
     root = None
     tree = Tree()
+    root = tree.insert(root, "@")
     root = tree.insert(root, "Lamda")
-    tree.insertLeft(root, "x")
-    tree.insertRight(root, "x")
-    print "\n"
-    print "Traverse Inorder"
+    root = tree.insert(root, "Lamda")
+    #Manually burrowing down to place data in the tree...
+    #The tree.insert() func needs to be rewrote to manually place items more than a level deep.
+    root.left.left = Node("f")
+    root.right.right = Node("+")
+    root.right.left = Node("y")
+    root.right.right.right = Node("1")
+    root.right.right.left = Node("y")
+    root.left.right = Node("Lamda")
+    root.left.right.left = Node("x")
+    root.left.right.right = Node("@")
+    root.left.right.right.left = Node("f")
+    root.left.right.right.right = Node("x")
+    #tree created... now doing something meaningful.
+
+
+    print "\nBEFORE:"
     tree.traverseInorder(root)
-    print tree.beta_redux_present(root)
+    
+    while(1):
+        if tree.beta_redux_present(root, False):
+            #print "\n\nHere... we ... go!\n\n"
+            root = tree.perform_beta_redux(root)
+            print "\nAFTER an Iteration:"
+            tree.traverseInorder(root)
+            #print "WE FOUND ONE!"
+            
+        else:
+            print "no beta redux present exiting...."
+            break
+
+    print "++++++++++++++++++++++\n\n"
 
 
 
--- trees.py
+++ trees.py
@@ -12,7 +12,7 @@
 #	for storing lamda calculus expressions.
 #***********************************************/
 
-
+import copy
 
 #A generic representation of a node to use in Tree below.
 class Node:
@@ -28,16 +28,21 @@ class Tree:
     def createNode(self, data):
         return Node(data)
     
-    #This will have to be tweaked for how to parse in input from expression.
+    #Will have to fix this guy for the parser, not sure this operates the way it needs to.
+    #Can create trees by using nodes themselves... [ex. root.right.left.data = Node("data")]
     def insert(self, node , data):
         if node is None:
             return self.createNode(data)
-        if data < node.data:
+        if node.left == None:
             node.left = self.insert(node.left, data)
-        elif data > node.data:
+            return node
+        elif node.right == None:
             node.right = self.insert(node.right, data)
-        return node
+            return node
+        node.left = self.insert(node.left, data)
+        node.right = self.insert(node.right, data)
     
+    #Only wrote these next two functions to manually place data in tree w/out haveing to parse.
     def insertLeft(self , node , data): 
         if node is None:
             return self.createNode(data)
@@ -50,43 +55,45 @@ class Tree:
         if node.right == None:
             node.right = self.insertRight(node.right, data)
 
-    def beta_redux_present(self, node):
+    #This function is called with arguements of (root_node, False) to test ... 
+    #...if any part of the entire tree contains a beta redux.
+    def beta_redux_present(self, node, prev_value):
+        return_value = prev_value
         if node is not None:
             if node.data == "@" and node.left.data == "Lamda":
-                return True
-            self.beta_redux_present(node.left)
-            self.beta_redux_present(node.right) 
-        return False
+                return_value = True
+            return_value = self.beta_redux_present(node.left, return_value)
+            return_value = self.beta_redux_present(node.right, return_value) 
+        return return_value
 
-    def search(self, node, data):
-        if node is None or node.data == data:
-            return node
-
-        if node.data < data:
-            return self.search(node.right, data)
-        else:
-            return self.search(node.left, data)
-
-    def deleteNode(self,node,data):
+    #This function is only called by perform_beta_redux()
+    def replacement(self, node, identifier, arguement):
         if node is None:
             return None
-        if data < node.data:
-            node.left = self.deleteNode(node.left, data)
-        elif data > node.data:
-            node.right = self.deleteNode(node.right, data)
-        else: 
-            if node.left is None and node.right is None:
-                del node
-            if node.left == None:
-                temp = node.right
-                del node
-                return  temp
-            elif node.right == None:
-                temp = node.left
-                del node
-                return temp
+        if node.data == identifier:
+            return arguement
+        node.left = self.replacement(node.left, identifier, arguement)
+        node.right = self.replacement(node.right, identifier, arguement)
+        
         return node
 
+    #This burrows down given node to LEFT first then RIGHT and finds a beta reduction...
+    #...performs it and then returns what should be returned given its location.
+    def perform_beta_redux(self, node):
+        if node is not None:
+            if node.data == "@" and node.left.data == "Lamda":
+                identifier = node.left.left.data
+                arguement = copy.deepcopy(node.right)
+                self.traverseInorder(arguement)
+                node = self.replacement(node.left.right, identifier, arguement)
+            else:
+                node.left = self.perform_beta_redux(node.left)
+                node.right = self.perform_beta_redux(node.right)
+
+        return node
+
+    #These are just tree printing functions.  Not pretty, but helps to see whats going on.
+    #A better method of illustrating a tree after a given iteration should prolly be looked at.
     def traverseInorder(self, root):
         if root is not None:
             self.traverseInorder(root.left)
