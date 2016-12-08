9c354e43d5b6d6820a725ca05f1dc5ccbc992867
dualhorn16 <dustin-horner@uiowa.edu>
2016-12-07 21:28:05 -0600

    Changed trees.py's node implementation completely
    Reworked what a node is, and how to set/access NodeTypes and values.
    Added latex print function to trees.py as well.  Updated beta_redux.py
    to test new implementations and latex output.

--- beta_redux.py
+++ beta_redux.py
@@ -14,6 +14,7 @@
 #***********************************************/
 
 from trees import *
+import codecs
 
 
 def main():
@@ -22,15 +23,17 @@ def main():
     #this expression is "(lamda x.x)y"
     tree = Tree()
     root = None
-    root = tree.insert(root, "@")
-    tree.insertLeft(root, "λ")
-    tree.insertRight(root, "y")
-    tree.insertLeft(root.left, "x")
-    tree.insertRight(root.left, "x")
+    root = tree.insert(root, NodeTypes.APPLICATION, None)
+    tree.insertLeft(root, NodeTypes.ABSTRACTION, None)
+    tree.insertRight(root, NodeTypes.VARIABLE, "y")
+    tree.insertLeft(root.left, NodeTypes.VARIABLE, "x")
+    tree.insertRight(root.left, NodeTypes.VARIABLE, "x")
     #tree created... now doing something meaningful.
     print "TREE MANUALLY ENTERED: (λx.x)y"
     print "\n"
     print "BEFORE:"
+    item_number = 1
+    tree.print_tree("output1.pdf", root, item_number)
     #print root
     tree.printNice(root, tree.height(root))
     #tree.traverseInorder(root)
@@ -41,6 +44,11 @@ def main():
     print "\nAFTER an Iteration:"
     #print root
     tree.printNice(root, tree.height(root))
+    tree.print_tree("output1.pdf", root, item_number+1)
+    output_file = codecs.open("output1.pdf", encoding='utf-8', mode='a')
+    output_file.write('\end{enumerate}\n')
+    output_file.write('\n\n\\end{document}\n')
+    output_file.close()
     #tree.traverseInorder(root)
     print "++++++++++++++++++++++\n\n"
 
@@ -50,25 +58,27 @@ def main():
     print "TREE MANUALLY ENTERED: (λf.λx.fx)λy.y+1"
     root = None
     tree = Tree()
-    root = tree.insert(root, "@")
-    root = tree.insert(root, "λ")
-    root = tree.insert(root, "λ")
+    root = tree.insert(root, NodeTypes.APPLICATION, None)
+    root = tree.insert(root, NodeTypes.ABSTRACTION, None)
+    root = tree.insert(root, NodeTypes.ABSTRACTION, None)
     #Manually burrowing down to place data in the tree...
     #The tree.insert() func needs to be rewrote to manually place items more than a level deep.
-    root.left.left = Node("f")
-    root.right.right = Node("+")
-    root.right.left = Node("y")
-    root.right.right.right = Node("1")
-    root.right.right.left = Node("y")
-    root.left.right = Node("λ")
-    root.left.right.left = Node("x")
-    root.left.right.right = Node("@")
-    root.left.right.right.left = Node("f")
-    root.left.right.right.right = Node("x")
+    root.left.left = Node(NodeTypes.VARIABLE, "f")
+    root.right.right = Node(NodeTypes.VARIABLE,"+")
+    root.right.left = Node(NodeTypes.VARIABLE,"y")
+    root.right.right.right = Node(NodeTypes.VARIABLE,"1")
+    root.right.right.left = Node(NodeTypes.VARIABLE,"y")
+    root.left.right = Node(NodeTypes.ABSTRACTION, None)
+    root.left.right.left = Node(NodeTypes.VARIABLE,"x")
+    root.left.right.right = Node(NodeTypes.APPLICATION, None)
+    root.left.right.right.left = Node(NodeTypes.VARIABLE,"f")
+    root.left.right.right.right = Node(NodeTypes.VARIABLE,"x")
     #tree created... now doing something meaningful.
 
 
     print "\nBEFORE:"
+    item_number = 1
+    tree.print_tree("output2.pdf", root, item_number)
     #print root
     #print 
     #print
@@ -80,9 +90,11 @@ def main():
         if tree.beta_redux_present(root, False):
             #print "\n\nHere... we ... go!\n\n"
             root = tree.perform_beta_redux(root)
+            item_number = item_number + 1
             print "\nAFTER an Iteration:"
             #tree.traverseInorder(root)
             tree.printNice(root, tree.height(root))
+            tree.print_tree("output2.pdf", root, item_number)
             #print root
             print "----------------------------"
             #print "WE FOUND ONE!"
@@ -91,28 +103,34 @@ def main():
             print "No more beta reductions present. Exiting...."
             break
 
+    output_file = codecs.open("output2.pdf", encoding='utf-8', mode='a')
+    output_file.write('\end{enumerate}\n')
+    output_file.write('\n\n\\end{document}\n')
+    output_file.close()
     print "++++++++++++++++++++++\n\n"
     #tree.printNice(root, tree.height(root))
     #print "\nHeight: ",tree.height(root)
 
     root = None
     tree = Tree()
-    root = tree.insert(root,"@")
-    root.left = Node("λ")
-    root.right = Node("@")
-    root.left.left = Node("x")
-    root.left.right = Node("+")
-    root.left.right.left = Node("x")
-    root.left.right.right = Node("1")
-    root.right.left = Node("λ")
-    root.right.right = Node("3")
-    root.right.left.left = Node("y")
-    root.right.left.right = Node("+")
-    root.right.left.right.left = Node("y")
-    root.right.left.right.right = Node("2")
+    root = tree.insert(root,NodeTypes.APPLICATION, None)
+    root.left = Node(NodeTypes.ABSTRACTION, None)
+    root.right = Node(NodeTypes.APPLICATION, None)
+    root.left.left = Node(NodeTypes.VARIABLE,"x")
+    root.left.right = Node(NodeTypes.VARIABLE,"+")
+    root.left.right.left = Node(NodeTypes.VARIABLE,"x")
+    root.left.right.right = Node(NodeTypes.VARIABLE,"1")
+    root.right.left = Node(NodeTypes.ABSTRACTION, None)
+    root.right.right = Node(NodeTypes.VARIABLE,"3")
+    root.right.left.left = Node(NodeTypes.VARIABLE,"y")
+    root.right.left.right = Node(NodeTypes.VARIABLE,"+")
+    root.right.left.right.left = Node(NodeTypes.VARIABLE,"y")
+    root.right.left.right.right = Node(NodeTypes.VARIABLE,"2")
     print "TREE MANUALLY ENTERED: (λx.x+1)((λy.y+2)3)"
 
     print "\nBEFORE:"
+    item_number = 1
+    tree.print_tree("output3.pdf", root, item_number)
     tree.printNice(root, tree.height(root))
     print "----------------------------"
     while(1):
@@ -122,6 +140,8 @@ def main():
             print "\nAFTER an Iteration:"
             #tree.traverseInorder(root)
             tree.printNice(root, tree.height(root))
+            item_number = item_number + 1
+            tree.print_tree("output3.pdf", root, item_number)
             #print root
             print "----------------------------"
             #print "WE FOUND ONE!"
@@ -131,6 +151,11 @@ def main():
             break
 
     print "++++++++++++++++++++++\n\n"
+
+    output_file = codecs.open("output3.pdf", encoding='utf-8', mode='a')
+    output_file.write('\end{enumerate}\n')
+    output_file.write('\n\n\\end{document}\n')
+    output_file.close()
 
 
 
--- trees.py
+++ trees.py
@@ -14,13 +14,48 @@
 #***********************************************/
 
 import copy
+import codecs
+import os
 
 #A generic representation of a node to use in Tree below.
-class Node:
-    def __init__(self, value):
+
+class NodeTypes(object):
+    '''
+    NodeTypes acts as enumerator for the 3 different kinds of nodes.
+    '''
+    ABSTRACTION = 1
+    APPLICATION = 2
+    VARIABLE = 3
+
+class Node(object):
+    '''
+    Object for parse tree.
+    '''
+    left = None
+    right = None
+    parent = None
+    node_type = None
+    value = None
+
+    def __init__(self, node_type, value):
         self.left = None
-        self.data = value
+        self.node_type = node_type
         self.right = None
+        if node_type == NodeTypes.VARIABLE:
+            self.value = value
+        else:
+            self.value = None
+
+    def to_string(self):
+        '''
+        Returns a string for the provided node
+        '''
+        if self.node_type == NodeTypes.ABSTRACTION:
+            return u'λ'
+        elif self.node_type == NodeTypes.APPLICATION:
+            return '@'
+        else:
+            return self.value
 
     def __str__(self, depth=0):
         ret = ""
@@ -30,7 +65,7 @@ class Node:
             ret += self.right.__str__(depth + 1)
 
         # Print own value
-        ret += "\n" + ("       "*depth) + str(self.data)
+        ret += "\n" + ("       "*depth) + str(self.to_string())
 
         # Print left branch
         if self.left != None:
@@ -39,67 +74,87 @@ class Node:
         return ret
 
 
-#A generic representation of a tree to use after parsing Lamda Calc Expression.
-class Tree:
 
-    def createNode(self, data):
-        return Node(data)
+class Tree:
+    '''
+    A generic representation of a tree to use after parsing Lamda Calc Expression.
+    '''
+    def createNode(self, node_type, value):
+        return Node(node_type, value)
     
-    #Will have to fix this guy for the parser, not sure this operates the way it needs to.
-    #Can create trees by using nodes themselves... [ex. root.right.left.data = Node("data")]
-    def insert(self, node , data):
+
+    def insert(self, node , node_type, value):
+        '''
+        Will have to fix this guy for the parser, not sure this operates the way it needs to.
+        Can create trees by using nodes themselves... [ex. root.right.left.data = Node("data")]
+        '''
         if node is None:
-            return self.createNode(data)
+            return self.createNode(node_type, value)
         if node.left == None:
-            node.left = self.insert(node.left, data)
+            node.left = self.insert(node.left, node_type, value)
             return node
         elif node.right == None:
-            node.right = self.insert(node.right, data)
+            node.right = self.insert(node.right, node_type, value)
             return node
-        node.left = self.insert(node.left, data)
-        node.right = self.insert(node.right, data)
+        node.left = self.insert(node.left, node_type, value)
+        node.right = self.insert(node.right, node_type, value)
     
-    #Only wrote these next two functions to manually place data in tree w/out haveing to parse.
-    def insertLeft(self , node , data): 
+
+    def insertLeft(self , node , node_type, value):
+        '''
+        Only wrote these next two functions to manually place data in tree w/out having to parse.
+        ''' 
         if node is None:
-            return self.createNode(data)
+            return self.createNode(node_type, value)
         if node.left == None:
-            node.left = self.insertLeft(node.left, data)
+            node.left = self.insertLeft(node.left, node_type, value)
 
-    def insertRight(self , node , data):
+    def insertRight(self , node , node_type, value):
+        '''
+        Only wrote these next two functions to manually place data in tree w/out haveing to parse.
+        '''
         if node is None:
-            return self.createNode(data)
+            return self.createNode(node_type, value)
         if node.right == None:
-            node.right = self.insertRight(node.right, data)
+            node.right = self.insertRight(node.right, node_type, value)
 
-    #This function is called with arguements of (root_node, False) to test ... 
-    #...if any part of the entire tree contains a beta redux.
+
     def beta_redux_present(self, node, prev_value):
+        '''
+        This function is called with arguements of (root_node, False) to test ... 
+        ...if any part of the entire tree contains a beta redux.
+        '''
         return_value = prev_value
         if node is not None:
-            if node.data == "@" and node.left.data == "λ":
+            if node.node_type == NodeTypes.APPLICATION and node.left.node_type == NodeTypes.ABSTRACTION:
                 return_value = True
             return_value = self.beta_redux_present(node.left, return_value)
             return_value = self.beta_redux_present(node.right, return_value) 
         return return_value
 
-    #This function is only called by perform_beta_redux()
+
     def replacement(self, node, identifier, arguement):
+        '''
+        This function is only called by perform_beta_redux()
+        '''
         if node is None:
             return None
-        if node.data == identifier:
+        if node.value == identifier:
             return arguement
         node.left = self.replacement(node.left, identifier, arguement)
         node.right = self.replacement(node.right, identifier, arguement)
         
         return node
 
-    #This burrows down given node to LEFT first then RIGHT and finds a beta reduction...
-    #...performs it and then returns what should be returned given its location.
+
     def perform_beta_redux(self, node):
+        '''
+        This burrows down given node to LEFT first then RIGHT and finds a beta reduction...
+        ...performs it and then returns what should be returned given its location.
+        '''
         if node is not None:
-            if node.data == "@" and node.left.data == "λ":
-                identifier = node.left.left.data
+            if node.node_type == NodeTypes.APPLICATION and node.left.node_type == NodeTypes.ABSTRACTION:
+                identifier = node.left.left.value
                 arguement = copy.deepcopy(node.right)
                 node = self.replacement(node.left.right, identifier, arguement)
             else:
@@ -108,28 +163,42 @@ class Tree:
 
         return node
 
-
+    
     def height(self, root):
+        '''
+        Returns the height of a tree/subtree from ROOT.  Used to get height for printNice() arguement.
+        '''
         if root is None:
             return 0
         else:
             return max(self.height(root.left), self.height(root.right)) + 1
 
-    #These are just tree printing functions.  Not pretty, but helps to see whats going on.
-    #A better method of illustrating a tree after a given iteration should prolly be looked at.
+
     def traverseInorder(self, root):
+        '''
+        These are just tree printing functions.  Not pretty, but helps to see whats going on.
+        A better method of illustrating a tree after a given iteration should prolly be looked at.
+        '''
         if root is not None:
             self.traverseInorder(root.left)
             print root.data
             self.traverseInorder(root.right)
 
     def traversePreorder(self, root):
+        '''
+        These are just tree printing functions.  Not pretty, but helps to see whats going on.
+        A better method of illustrating a tree after a given iteration should prolly be looked at.
+        '''
         if root is not None:
             print root.data
             self.traversePreorder(root.left)
             self.traversePreorder(root.right)
 
     def traversePostorder(self, root):
+        '''
+        These are just tree printing functions.  Not pretty, but helps to see whats going on.
+        A better method of illustrating a tree after a given iteration should prolly be looked at.
+        '''
         if root is not None:
             self.traversePreorder(root.left)
             self.traversePreorder(root.right)
@@ -138,7 +207,7 @@ class Tree:
     def printNice(self, rootnode, height):
         thislevel = [rootnode]
         level = 1
-        a = "                "
+        a = "                                "
         a2 = "================"
         while thislevel:
             nextlevel = list()
@@ -146,13 +215,13 @@ class Tree:
             a = a[:len(a)/2]
             for n in thislevel:
                 if level is 1:
-                    print a[:len(a)-1]+n.data,
+                    print a[:len(a)-3]+n.to_string(),
                 else:
-                    print a+n.data,
+                    print a+n.to_string(),
                 if n.left: nextlevel.append(n.left),nextlevel2.append("/") 
-                if not n.left and level < height: nextlevel.append(Node(a[:(len(a)/4)])),nextlevel2.append(a[:(len(a)/4)])
+                if not n.left and level < height: nextlevel.append(Node(NodeTypes.VARIABLE, a[:(len(a)/4)])),nextlevel2.append(a[:(len(a)/4)])
                 if n.right: nextlevel.append(n.right),nextlevel2.append("\\")
-                if not n.right and level < height: nextlevel.append(Node(a[:(len(a)/4)])),nextlevel2.append(a[:(len(a)/4)])
+                if not n.right and level < height: nextlevel.append(Node(NodeTypes.VARIABLE, a[:(len(a)/4)])),nextlevel2.append(a[:(len(a)/4)])
             print
             for m in nextlevel2:
                 if m is "/":
@@ -164,6 +233,76 @@ class Tree:
             print
             level=level+1
             thislevel = nextlevel
+
+    def print_tree(self, filename, node, item):
+        '''
+        Prints the parse tree using LaTex and QTree
+        '''
+        if (os.path.exists(filename)):
+            try:
+                output_file = codecs.open(filename, encoding='utf-8', mode='a')
+            except IOError:
+                print('cannot write to file:', filename)
+                return
+
+        else:
+            try:
+                output_file = codecs.open(filename, encoding='utf-8', mode='w')
+            except IOError:
+                print('cannot write to file:', filename)
+                return
+            # begin file with headers
+            output_file.write('\\documentclass[utf8]{article}\n')
+            output_file.write('\\usepackage{qtree}\n')
+            output_file.write('\\begin{document}\n')
+            output_file.write('\\textbf{Reduction Sequence:}\n')
+            output_file.write('\\begin{enumerate}\n')
+            #output_file.write('\\qtreecenterfalse\n')
+            output_file.write('\\item[('+str(item)+')] ')
+
+        character = chr(96 + item)
+        output_file.write(character + '. ')
+        output_file.write('\\Tree ')
+        # Traverse tree here
+        self.print_tree_rec(output_file, node, 1)
+        # complete file
+        output_file.write('\n')
+        output_file.write('\hskip 0.3in')
+        output_file.close()
+        return
+            
+
+    def print_tree_rec(self, output_file, node, level):
+        '''
+        Recursive function for print_tree()
+        '''
+        # A node has either 0 or 2 children. If 0, just output it's value (it must be a variable)
+        # If level is 1 and no children... its a 1 node tree (just root)
+        if node.left is None:
+            if level == 1:
+                output_file.write('[')
+                output_file.write('.')
+            output_file.write(node.to_string())
+            if level == 1:
+                output_file.write(' ')
+                output_file.write(']')
+
+        # If node has children, need to use brackets and period, and recurse
+        else:
+            
+            output_file.write('[')
+            output_file.write('.')
+            if node.node_type == NodeTypes.ABSTRACTION:
+                output_file.write('$\\lambda$')
+            else:
+                output_file.write(node.to_string())
+            output_file.write(' ')
+            self.print_tree_rec(output_file, node.left, level+1)
+            output_file.write(' ')
+            self.print_tree_rec(output_file, node.right, level+1)
+            output_file.write(' ')
+            output_file.write(']')
+
 
 
 
